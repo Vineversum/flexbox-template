9bdcc82eeddd116b8b341c2fde69cda3595d6d99
Vineversum <Vineversum@gmail.com>
2019-07-26 01:32:41 +0300

    Сделал выпадающее меню

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
       $('body').niceScroll();
   });
 
-  $('.header-menu__btn').click(function() {
+  $('.header-menu__button').click(function() {
     $(this).toggleClass('active');
     $('.header-menu__list').toggleClass('active');
   });
